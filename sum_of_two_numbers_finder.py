07da8c1cf7aef200109bc42521ee77b71720a952
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-11 21:31:25 +0800

    created a snippet to display the sum of the two numbers inputed by the user

--- sum_of_two_numbers_finder.py
+++ sum_of_two_numbers_finder.py
@@ -2,3 +2,6 @@ number_one = float(input("enter a number: "))
 number_two = float(input("enter another number: "))
 
 sum = float(number_one + number_two)
+
+print("the sum of", float(number_one), "and",
+      float(number_two), "is equal to", sum)
