a1559a22f4211f189a0666d9691bee54ba9c2528
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-11 22:37:39 +0800

    converted the list into tuple

--- odd_number_counter.py
+++ odd_number_counter.py
@@ -2,5 +2,6 @@ ten_numbers = []
 
 for i in range(10):
     given_numbers = float(input("enter ten numbrs: "))
+    ten_numbers.append(given_numbers)
 
-given_numbers = tuple(ten_numbers)
+entered_numbers = tuple(ten_numbers)
