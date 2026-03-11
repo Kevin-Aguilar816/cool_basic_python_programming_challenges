69ab45cf12186f97a26e2ac3afbd1ca8135a22d0
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-11 19:46:43 +0800

    added_a_function_where_if_the_first_number_is_greater_than_the_second_number_display_that_the_first_number_is_greater_than_the_second_number

--- bigger_number_finder.py
+++ bigger_number_finder.py
@@ -1,2 +1,5 @@
 number_one = float(input("enter a number: "))
 number_two = float(input("enter another number: "))
+
+if number_one > number_two:
+    print(float(number_one), "is greater than", float(number_two))
