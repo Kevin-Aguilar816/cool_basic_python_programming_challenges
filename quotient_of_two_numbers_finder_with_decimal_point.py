17b42ae833a3660cf737bea4d3d773c35e8f3853
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-11 21:56:56 +0800

    changed the input string from first 'number' to 'dividend' and from second 'number' to 'divisor'

--- quotient_of_two_numbers_finder_with_decimal_point.py
+++ quotient_of_two_numbers_finder_with_decimal_point.py
@@ -1,5 +1,5 @@
-number_one = float(input("enter a number: "))
-number_two = float(input("enter another number: "))
+number_one = float(input("enter a dividend: "))
+number_two = float(input("enter a divisor: "))
 
 quotient = number_one / number_two
 
