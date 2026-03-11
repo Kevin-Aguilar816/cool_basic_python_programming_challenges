229002267edff5f1016f889fa9790367d0938ba9
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-11 22:11:17 +0800

    created a snippet where the program displays the result when the base is raised by its exponent

--- exponent_calculator.py
+++ exponent_calculator.py
@@ -2,3 +2,5 @@ number_one = float(input("enter the base: "))
 number_two = float(input("enter the exponent: "))
 
 result = number_one ** number_two
+
+print(float(number_one), "raised to", float(number_two), "is equal to", result)
