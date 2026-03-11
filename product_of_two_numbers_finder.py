b918154cbddbc874c77fd853ac83cdf6156dfb4b
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-11 21:38:37 +0800

    created a snippet to display the product of the two given numbers of the user

--- product_of_two_numbers_finder.py
+++ product_of_two_numbers_finder.py
@@ -2,3 +2,6 @@ number_one = float(input("enter a number: "))
 number_two = float(input("enter another number: "))
 
 product = number_one * number_two
+
+print("the product of", float(number_one), "and",
+      float(number_two), "is equal to", float(product))
