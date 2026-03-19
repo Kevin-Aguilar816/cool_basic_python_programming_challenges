e0c865af30f37bcb3f1eb84217db9ff88ad35a71
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 14:57:33 +0800

    created an else statement to print that the two numbers inputed by the user is equal

--- inequal_number_identifier.py
+++ inequal_number_identifier.py
@@ -3,3 +3,5 @@ second_number = float(input("enter the second number: "))
 
 if first_number != second_number:
     print(float(first_number), "is not equal to", float(second_number))
+else:
+    print(float(first_number), "is equal to", float(second_number))
