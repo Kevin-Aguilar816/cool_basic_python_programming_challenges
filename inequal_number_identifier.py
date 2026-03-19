00c0ef9ba02c101507fc57e86229df962b207109
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 14:56:16 +0800

    created an if statement that shows not equal if the first number and the second number is not the same

--- inequal_number_identifier.py
+++ inequal_number_identifier.py
@@ -1,2 +1,5 @@
 first_number = float(input("enter the first number: "))
 second_number = float(input("enter the second number: "))
+
+if first_number != second_number:
+    print(float(first_number), "is not equal to", float(second_number))
