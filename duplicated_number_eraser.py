6e929077672aa1ab6be1de822cca9f5138e170a6
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 07:16:51 +0800

    created a line that displays all the unique numbers as well as the duplicated numbers without their duplicates

--- duplicated_number_eraser.py
+++ duplicated_number_eraser.py
@@ -8,3 +8,5 @@ unique_numbers = []
 for given_numbers in ten_numbers:
     if given_numbers not in unique_numbers:
         unique_numbers.append(given_numbers)
+
+print("\nNumbers without their duplicates:", unique_numbers)
