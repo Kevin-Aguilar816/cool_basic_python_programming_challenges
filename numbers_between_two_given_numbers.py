f673225c600e3e57dcfa8417841068b4694e1f99
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 17:34:03 +0800

    created a snippet to determine the integers between the givens with an increment of one integer each

--- numbers_between_two_given_numbers.py
+++ numbers_between_two_given_numbers.py
@@ -5,3 +5,11 @@ start = min(number_one, number_two)
 end = max(number_one, number_two)
 
 print(f"\nNumbers between {start} and {end}:")
+
+if start.is_integer() and end.is_integer():
+    start = int(start)
+    end = int(end)
+    current = start + 1
+    while current < end:
+        print(current, end=" ")
+        current += 1
