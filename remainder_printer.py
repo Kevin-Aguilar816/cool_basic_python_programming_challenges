74b5a3866f55aa7a9ff32b62f24577b9871c345c
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 15:30:54 +0800

    created a prompt to print the remainder of the two given numbers by the user

--- remainder_printer.py
+++ remainder_printer.py
@@ -2,3 +2,6 @@ first_number = float(input("enter the dividend: "))
 second_number = float(input("enter the divisor: "))
 
 remainder = first_number % second_number
+
+print("the remainder of", float(first_number),
+      "/", float(second_number), "=", remainder)
