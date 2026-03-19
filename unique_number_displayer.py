60f44edc246cc76c36e1f78b8d14fc9e0d3f9553
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 06:52:14 +0800

    created a snippet that determines the unique numbers entered by the user

--- unique_number_displayer.py
+++ unique_number_displayer.py
@@ -3,3 +3,8 @@ print("Enter ten numbers:")
 for i in range(10):
     given_numbers = float(input(f"Number {i+1}: "))
     ten_numbers.append(given_numbers)
+
+unique_numbers = []
+for given_numbers in ten_numbers:
+    if given_numbers not in unique_numbers:
+        unique_numbers.append(given_numbers)
