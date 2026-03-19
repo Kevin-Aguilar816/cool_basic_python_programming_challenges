1bc7b87dbdd4c07a1bf6f0e2074d0e0f354e307e
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 07:03:46 +0800

    created a new snippet to correctly display only the numbers that do not have any duplicate to make the program correct

--- unique_number_displayer.py
+++ unique_number_displayer.py
@@ -1,10 +1,9 @@
+from collections import Counter
 ten_numbers = []
 print("Enter ten numbers:")
 for i in range(10):
-    given_numbers = float(input(f"Number {i+1}: "))
+    given_numbers = int(input(f"Number {i+1}: "))
     ten_numbers.append(given_numbers)
 
-unique_numbers = []
-for given_numbers in ten_numbers:
-    if given_numbers not in unique_numbers:
-        unique_numbers.append(given_numbers)
+count = Counter(ten_numbers)
+unique_numbers = [num for num, cnt in count.items() if cnt == 1]
