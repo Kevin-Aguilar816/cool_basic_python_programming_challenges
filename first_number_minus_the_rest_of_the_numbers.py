cd8688762c896368a58183d453fa9fc9c61329d1
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 15:56:19 +0800

    set the first number

--- first_number_minus_the_rest_of_the_numbers.py
+++ first_number_minus_the_rest_of_the_numbers.py
@@ -3,3 +3,5 @@ print("please enter ten numbers: ")
 for i in range(10):
     given_numbers = float(input(f"enter number {i+1}: "))
     ten_numbers.append(given_numbers)
+
+first_number = ten_numbers[0]
