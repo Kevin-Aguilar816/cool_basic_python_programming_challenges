f8312ddb5f49efb1fb24491d1b904f305698adb8
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 14:43:23 +0800

    created an elif statement that prints the first number if it is smaller than the second number

--- smaller_number_identifier.py
+++ smaller_number_identifier.py
@@ -3,3 +3,6 @@ second_number = float(input("enter the second number: "))
 
 if first_number > second_number:
     print(float(second_number), "is the smaller number.")
+
+elif first_number < second_number:
+    print(float(first_number), "is the smaller number.")
