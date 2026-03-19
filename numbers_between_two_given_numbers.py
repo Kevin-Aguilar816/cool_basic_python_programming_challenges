f77aec33a6ddc82021082a695728aaa42062d8a2
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 17:29:13 +0800

    created a snippet that prints the lesser number first then followed by the larger

--- numbers_between_two_given_numbers.py
+++ numbers_between_two_given_numbers.py
@@ -1,2 +1,7 @@
 number_one = float(input("Enter first number: "))
 number_two = float(input("Enter second number: "))
+
+start = min(number_one, number_two)
+end = max(number_one, number_two)
+
+print(f"\nNumbers between {start} and {end}:")
