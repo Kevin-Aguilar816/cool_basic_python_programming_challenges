144496385bfaa89f1ea35d7821b42c431bc766da
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 15:08:20 +0800

    created a prompt to print the difference of the two numbers given by the user

--- difference_solver.py
+++ difference_solver.py
@@ -2,3 +2,5 @@ first_number = float(input("enter the minuend: "))
 second_number = float(input("enter the subtrahend: "))
 
 difference = float(first_number) - float(second_number)
+
+print(float(first_number), "-", float(second_number), "=", difference)
