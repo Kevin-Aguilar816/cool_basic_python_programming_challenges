5d247aef76116dfe588d5805dd82bde9f2ffa041
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 16:35:07 +0800

    fixed the error and define first the variable even_number_count then proceed on making a snippet to print the count of even number

--- even_number_counter.py
+++ even_number_counter.py
@@ -6,6 +6,10 @@ for i in range(10):
 
 entered_numbers = tuple(ten_numbers)
 
+even_number_count = 0
+
 for i in entered_numbers:
     if i % 2 == 0:
         even_number_count += 1
+
+print("count of even numbers:", even_number_count)
