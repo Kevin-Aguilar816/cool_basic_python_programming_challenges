ed91a2a2d565c4c9f16d1d1394c13556d84e2a17
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 15:16:56 +0800

    created a prompt to print the quotient of the two numbers without giving decimal point

--- quotient_without_decimal_point_solver.py
+++ quotient_without_decimal_point_solver.py
@@ -2,3 +2,5 @@ first_number = float(input("enter the dividend: "))
 second_number = float(input("enter the divisor: "))
 
 quotient = float(first_number) // float(second_number)
+
+print(float(first_number), "/", float(second_number), "=", quotient)
