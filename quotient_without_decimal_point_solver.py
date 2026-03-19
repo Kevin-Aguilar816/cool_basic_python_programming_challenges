9651746e951a90c035a45b1a063ff2db0ff3706f
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 15:15:04 +0800

    changed line 4 variable name to quotient

--- quotient_without_decimal_point_solver.py
+++ quotient_without_decimal_point_solver.py
@@ -1,4 +1,4 @@
 first_number = float(input("enter the dividend: "))
 second_number = float(input("enter the divisor: "))
 
-floor_division = float(first_number) // float(second_number)
+quotient = float(first_number) // float(second_number)
