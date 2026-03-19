7c98a5470f08fd5d865253269219234d7e14a910
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-19 17:12:21 +0800

    created a snippet that puts a comma for each number that will be printed for readability as well as determine the totality of all the numbers to be printed

--- numbers_from_zero_to_one_hundred_excluding_numbers_ending_in_zero_or_five.py
+++ numbers_from_zero_to_one_hundred_excluding_numbers_ending_in_zero_or_five.py
@@ -5,3 +5,10 @@ count = 0
 while number <= 100:
     last_digit = number % 10
     if last_digit != 0 and last_digit != 5:
+        if not first:
+            print(",", end=" ")
+        print(number, end="")
+        first = False
+        count += 1
+
+    number += 1
