8444032669253c917fe7514303744f9cda9632dd
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 23:24:37 +0800

    created lines to ask the user to input text and the suffix to crosscheck if it matches or not

--- custom_endswith.py
+++ custom_endswith.py
@@ -2,3 +2,8 @@ def custom_endswith(text, suffix):
     if len(text) >= len(suffix):
         return text[-len(suffix):] == suffix
     return False
+
+
+text = input("Enter text: ")
+suffix = input("Enter suffix: ")
+print("Output:", custom_endswith(text, suffix))
