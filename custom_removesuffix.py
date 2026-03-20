a88117723f5e1a49a126434da213898fbc58c7ed
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:15:58 +0800

    created lines for user to enter text, the suffix to be removed, and print the output, as well as added an equal sign on line two

--- custom_removesuffix.py
+++ custom_removesuffix.py
@@ -1,4 +1,9 @@
 def custom_removesuffix(text, suffix):
-    if len(text) >= len(suffix) and text[-len(suffix):] = suffix:
+    if len(text) >= len(suffix) and text[-len(suffix):] == suffix:
         return text[:-len(suffix)]
     return text
+
+
+text = input("Enter text: ")
+suffix = input("Enter suffix to remove: ")
+print("Output:", custom_removesuffix(text, suffix))
