7e463329e16eef35b49f2c4d82eaf00c5d92a654
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 21:33:56 +0800

    Created a block of code that gets the sum of the entered numbers and divide it by its length to get the average

--- average_calculator_of_list_that_can_be_terminated_if_non_number_character_is_entered.py
+++ average_calculator_of_list_that_can_be_terminated_if_non_number_character_is_entered.py
@@ -5,3 +5,6 @@ while True:
     try:
         given_numbers = float(input("Enter number: "))
         numbers.append(given_numbers)
+    except ValueError:
+        if numbers:
+            average = sum(numbers) / len(numbers)
