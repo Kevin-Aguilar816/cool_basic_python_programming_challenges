f7fd98d0bfe76c772bb1ebac503ad4bb34feb2f6
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 22:45:45 +0800

    created lines to turn the full name entered by the user into snake case then print it

--- snake_casing_of_incorrect_case_full_name.py
+++ snake_casing_of_incorrect_case_full_name.py
@@ -1,1 +1,3 @@
 full_name = input("Enter your full name with incorrect case: ")
+snake_case_full_name = "_".join(word.lower() for word in full_name.split())
+print("full name in snake case:", snake_case_full_name)
