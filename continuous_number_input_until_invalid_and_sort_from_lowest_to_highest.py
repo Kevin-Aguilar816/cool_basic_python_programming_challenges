9484adceac226a6b414aeddde099b744f8f050bc
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 08:39:28 +0800

    created an else block to print no numbers entered if the first input is already a non-number and fixed the syntax error of line six from inpput to input

--- continuous_number_input_until_invalid_and_sort_from_lowest_to_highest.py
+++ continuous_number_input_until_invalid_and_sort_from_lowest_to_highest.py
@@ -3,9 +3,12 @@ print("Enter numbers (non-number to stop):")
 
 while True:
     try:
-        num = float(inpput("Enter number: "))
+        num = float(input("Enter number: "))
         numbers.append(num)
     except ValueError:
         if numbers:
             numbers.sort()
             print("Sorted numbers (lowest to highest):", numbers)
+        else:
+            print("No numbers entered.")
+        break
