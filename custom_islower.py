f56ab79a7541a1125f0d473bd9c1f01b22e1faef
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:23:36 +0800

    create lines for user to input text and print the output

--- custom_islower.py
+++ custom_islower.py
@@ -3,3 +3,7 @@ def custom_islower(text):
         if char.isalpha() and not ('a' <= char <= 'z'):
             return False
     return True
+
+
+text = input("Enter text: ")
+print("Output:", custom_islower(text))
