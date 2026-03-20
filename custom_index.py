5abfe6a50dcdff705135fdf8fc378e550fa03b55
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:38:47 +0800

    created block of code that replicate the function of custom index

---
+++ custom_index.py
@@ -0,0 +1,11 @@
+def custom_index(text, substring):
+    for i in range(len(text) - len(substring) + 1):
+        if text[i:i+len(substring)] == substring:
+            return i
+    raise ValueError("substring not found")
+
+
+text = input("Enter text: ")
+substr = input("Enter substring: ")
+try:
+    print("Output:", custom_index(text, substr))
