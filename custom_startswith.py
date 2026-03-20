c77cdeeb2ddad3521d921704e696219ddac9d982
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:27:29 +0800

    created lines for user to enter text, the prefix, and print if it is correct

--- custom_startswith.py
+++ custom_startswith.py
@@ -2,3 +2,8 @@ def custom_startswith(text, prefix):
     if len(text) >= len(prefix):
         return text[:len(prefix)] == prefix
     return False
+
+
+text = input("Enter text: ")
+prefix = input("Enter prefix: ")
+print("Output:", custom_startswith(text, prefix))
