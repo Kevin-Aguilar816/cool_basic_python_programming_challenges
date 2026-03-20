7165c49a65a5a33f82dd959d2413e6da7a06e11b
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 21:25:05 +0800

    created a block of code to sort the numbers entered from highest to lowest

--- highest_to_lowest_number_sorting_on_a_list_that_can_be_terminated_by_entering_a_non_number_character.py
+++ highest_to_lowest_number_sorting_on_a_list_that_can_be_terminated_by_entering_a_non_number_character.py
@@ -5,3 +5,7 @@ while True:
     try:
         given_numbers = float(input("Enter a number: "))
         numbers.append(given_numbers)
+    except ValueError:
+        if numbers:
+            numbers.sort(reverse=True)
+            print("Numbers (highest to lowest):", numbers)
