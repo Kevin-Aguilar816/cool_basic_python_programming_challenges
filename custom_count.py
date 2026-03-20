7af8f029ab5daf2c613dbfeb9f6ec0e2e7d42bc2
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:37:12 +0800

    created lines for user to input text, substring, and print the output

--- custom_count.py
+++ custom_count.py
@@ -8,3 +8,8 @@ def custom_count(text, substring):
         count += 1
         start = pos + 1
     return count
+
+
+text = input("Enter text: ")
+substr = input("Enter substring: ")
+print("Output:", custom_count(text, substr))
