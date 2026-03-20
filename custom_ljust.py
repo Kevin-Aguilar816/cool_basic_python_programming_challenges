c80768488c0a8277045382379acf0813b9062790
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 23:36:12 +0800

    created lines that asks the user for text, its desired width, and prints it ultimately

--- custom_ljust.py
+++ custom_ljust.py
@@ -3,3 +3,8 @@ def custom_ljust(text, width):
     while len(result) < width:
         result += " "
     return result
+
+
+text = input("Enter text: ")
+width = int(input("Enter width: "))
+print("Output:", repr(custom_ljust(text, width)))
