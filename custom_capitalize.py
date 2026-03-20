9830d462b248d3d69b4e2a8309553da3cced675c
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:04:51 +0800

    created a new block of code for the user to enter text and print a correct output

--- custom_capitalize.py
+++ custom_capitalize.py
@@ -1,3 +1,13 @@
+def custom_lower(text):
+    result = ""
+    for char in text:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+    return result
+
+
 def custom_capitalize(text):
     if len(text) == 0:
         return ""
@@ -5,3 +15,7 @@ def custom_capitalize(text):
     if len(result) > 0:
         result = result[0].upper() + result[1:]
     return result
+
+
+text = input("Enter text: ")
+print("Output:", custom_capitalize(text))
