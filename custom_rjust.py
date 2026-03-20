3070cfe8f4380ca8a15cc17a8e071f2153564350
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:30:51 +0800

    created lines for user to input text, desired width, and the intended output

--- custom_rjust.py
+++ custom_rjust.py
@@ -2,3 +2,8 @@ def custom_rjust(text, width):
     result = text
     spaces_needed = width - len(text)
     return " " * spaces_needed + result
+
+
+text = input("Enter text: ")
+width = int(input("Enter width: "))
+print("Output:", repr(custom_rjust(text, width)))
