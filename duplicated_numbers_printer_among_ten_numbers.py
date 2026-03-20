60149fb1f204c8ef8d3f7493c0d539d623bea79d
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 20:56:18 +0800

    created a new line on line 6 to append the input to the empty list as well as coded two lines to show the duplicated numbers as well as the totality of all numbers

--- duplicated_numbers_printer_among_ten_numbers.py
+++ duplicated_numbers_printer_among_ten_numbers.py
@@ -3,7 +3,11 @@ ten_numbers = []
 print("Enter ten numbers:")
 for i in range(10):
     given_numbers = float(input(f"Number {i+1}: "))
+    ten_numbers.append(given_numbers)
 
 count = Counter(ten_numbers)
 duplicated_numbers = [given_numbers for given_numbers,
                       cnt in count.items() if cnt > 1]
+
+print("\nNumbers with duplicates:", duplicated_numbers)
+print("All numbers:", ten_numbers)
