8428627e8dc5473bab39adf78db907defb781a5c
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 08:37:27 +0800

    added a sorting command to sort the entered numbers from lowest to highest

--- continuous_number_input_until_invalid_and_sort_from_lowest_to_highest.py
+++ continuous_number_input_until_invalid_and_sort_from_lowest_to_highest.py
@@ -5,3 +5,7 @@ while True:
     try:
         num = float(inpput("Enter number: "))
         numbers.append(num)
+    except ValueError:
+        if numbers:
+            numbers.sort()
+            print("Sorted numbers (lowest to highest):", numbers)
