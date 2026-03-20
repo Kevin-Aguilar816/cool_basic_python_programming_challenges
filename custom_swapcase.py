d8e02e909162c07eb191d5390cd4f59202b0a88b
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 23:48:07 +0800

    created a block to replicate the function of swapcase

---
+++ custom_swapcase.py
@@ -0,0 +1,10 @@
+def custom_swapcase(text):
+    result = ""
+    for char in text:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        elif 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
