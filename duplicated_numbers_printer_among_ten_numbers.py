1241ff090a932cd581c94fe0d74688aa45a230f9
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 20:51:06 +0800

    created a block of code to identify if there are any duplicated numbers

--- duplicated_numbers_printer_among_ten_numbers.py
+++ duplicated_numbers_printer_among_ten_numbers.py
@@ -1,4 +1,9 @@
+from collections import Counter
 ten_numbers = []
 print("Enter ten numbers:")
 for i in range(10):
     given_numbers = float(input(f"Number {i+1}: "))
+
+count = Counter(ten_numbers)
+duplicated_numbers = [given_numbers for given_numbers,
+                      cnt in count.items() if cnt > 1]
