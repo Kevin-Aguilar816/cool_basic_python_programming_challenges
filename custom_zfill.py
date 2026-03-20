5aad611d9191ddf03a9f3f8fde4c1f3952e687d2
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-21 00:33:47 +0800

    created lines for user to input text, desired width, and print the outcome

--- custom_zfill.py
+++ custom_zfill.py
@@ -2,3 +2,8 @@ def custom_zfill(text, width):
     result = text
     zeros_needed = width - len(text)
     return "0" * zeros_needed + result
+
+
+text = input("Enter text: ")
+width = int(input("Enter width: "))
+print("Output:", custom_zfill(text, width))
