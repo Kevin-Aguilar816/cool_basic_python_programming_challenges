993f83eaa966314c4638b24faa192a03a50c76ae
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 21:04:16 +0800

    created a block of code that count the most duplicated number in the list

--- most_frequent_duplicated_number_on_a_terminating_list_if_non_number_character_is_entered.py
+++ most_frequent_duplicated_number_on_a_terminating_list_if_non_number_character_is_entered.py
@@ -7,3 +7,7 @@ while True:
     try:
         given_numbers = float(input("Enter number: "))
         numbers.append(given_numbers)
+    except ValueError:
+        if numbers:
+            count = Counter(numbers)
+            most_common_number = count.most_common(1)[0]
