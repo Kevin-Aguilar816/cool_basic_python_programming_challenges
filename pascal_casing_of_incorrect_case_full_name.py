eca0241a0924d007911137a76029732bc393de9b
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-03-20 22:38:57 +0800

    created lines of code to make the entered full name to turn into a pascal casing as well as print it

--- pascal_casing_of_incorrect_case_full_name.py
+++ pascal_casing_of_incorrect_case_full_name.py
@@ -1,1 +1,3 @@
 full_name = input("Enter your full name in incorrect case: ")
+pascal_casing_full_name = "".join(word.title() for word in full_name.split())
+print("full name:", pascal_casing_full_name)
